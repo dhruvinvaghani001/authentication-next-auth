d5a0ce2daaf9b55958070e21114136af0e4c1ce9
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2024-05-15 14:04:18 +0530

    folder structure modify and session provider added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Provider from "./component/Provider";
+import Provider from "../component/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
