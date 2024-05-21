6e612cd349d6da504873c9e730c42b9527d0a092
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2024-05-21 18:30:01 +0530

    authform added for login and signup

--- src/components/auth/AuthForm.jsx
+++ src/components/auth/AuthForm.jsx
@@ -6,9 +6,12 @@ import Button from "../ui/Button";
 import Input from "../ui/Input";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import { DevTool } from "@hookform/devtools";
+import toast, { Toaster } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
 
-const AuthForm = () => {
+const AuthForm = ({ type }) => {
+  const isLoginForm = type == "login";
   const form = useForm();
   const {
     register,
@@ -16,40 +19,67 @@ const AuthForm = () => {
     formState: { errors },
     control,
   } = form;
+  const router = useRouter();
 
   const onSubmit = async (data) => {
-    console.log(data);
-    const formdata = new FormData();
-    for (let key in data) {
-      formdata.append(key, data[key]);
-    }
-    try {
-      const res = await axios.post("/signup", formdata);
-      console.log(res);
-    } catch (error) {
-      console.log(error);
+    if (isLoginForm) {
+      try {
+        const res = await signIn("credentials", {
+          email: data.email,
+          password: data.password,
+          redirect: false,
+        });
+        if (!res.ok) {
+          toast.error(res.error);
+        }
+        if (res.ok) {
+          toast.success("User loged in Successfully!");
+          router.push("/");
+        }
+      } catch (error) {
+        toast.error(error);
+      }
+    } else {
+      try {
+        const res = await axios.post("/api/signup", {
+          email: data.email,
+          username: data.username,
+          password: data.password,
+        });
+        if (res.status == 200) {
+          console.log(res);
+          toast.success(res.data.message);
+          router.push(`/verify/${res.data.username}`);
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error(error.response.data.message);
+      }
     }
   };
-
-  console.log(errors);
   return (
     <div>
+      <div>
+        <Toaster />
+      </div>
       <form
         className="space-y-4 md:space-y-6"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div>
-          <Input
-            label="username"
-            type="text"
-            id="username"
-            placeholder="jhon doe"
-            {...register("username", { required: "Username is required !" })}
-          />
-          {errors.username && (
-            <p className="text-red-500 mt-1">{errors.username.message}</p>
-          )}
-        </div>
+        {!isLoginForm && (
+          <div>
+            <Input
+              label="username"
+              type="text"
+              id="username"
+              placeholder="jhon doe"
+              {...register("username", { required: "Username is required !" })}
+            />
+            {errors.username && (
+              <p className="text-red-500 mt-1">{errors.username.message}</p>
+            )}
+          </div>
+        )}
         <div>
           <Input
             label="email"
@@ -74,18 +104,19 @@ const AuthForm = () => {
             <p className="text-red-500 mt-1">{errors.password.message}</p>
           )}
         </div>
-        <Button type="submit">Create a Account</Button>
+        <Button type="submit">
+          {isLoginForm ? "Login" : "Create a Account"}
+        </Button>
         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-          Already have an account?{" "}
+          {isLoginForm ? "Don't have an account? " : "Already have an account?"}
           <Link
-            href="/login"
+            href={isLoginForm ? "/signup" : "/signin"}
             className="font-medium text-primary-600 hover:underline dark:text-primary-500"
           >
-            Login here
+            {isLoginForm ? "Create Account" : " Login here"}
           </Link>
         </p>
       </form>
-      <DevTool control={control} />
     </div>
   );
 };
