348f975637315c36fcecdb871f6f76fb33217d3f
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2024-05-22 14:17:19 +0530

    button ui changes

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Button = ({ type, onClick, children }) => {
+const Button = ({ type, onClick, className, children }) => {
   return (
     <>
       <button
+        onClick={onClick}
         type={type}
-        className="w-full text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        className={` w-full text-white font-medium rounded-lg  text-center text-sm px-5 py-2.5 flex justify-center items-center gap-10 ${className}`}
       >
         {children}
       </button>
